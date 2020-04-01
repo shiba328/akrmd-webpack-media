c55524e195bd2c3e05948ab37464ccd23325e3f8
shiba328 <mitue.okamoto@gmail.com>
2020-04-01 18:38:41 +0900

    feat: webpack html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlwebpackPlugin = require("html-webpack-plugin");
 const { GenerateSW } = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   plugins: [
     new HtmlwebpackPlugin({
       template: `./index.html`
+    }),
+    new HtmlwebpackPlugin({
+      template: path.join(__dirname, `about.html`),
+      filename: 'about.html'
     }),
     new GenerateSW()
   ],
